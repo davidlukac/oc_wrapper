b1b87ea04b05582efe681a11b2e8cd893761e851
David Lukac <david.lukac@ferratum.com>
2019-01-29 17:24:27 +0100

    Update missing dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oc_wrapper",
-    version="0.1.0-dev2",
+    version="0.1.0-dev3",
     author="David Lukac",
     author_email="david.lukac@gmail.com",
     description="Python wrapper for OpenShift CLI (oc) command line tool.",
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/davidlukac/oc_wrapper",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'python-dateutil',
+        'pytz',
+        'typing',
+    ],
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
